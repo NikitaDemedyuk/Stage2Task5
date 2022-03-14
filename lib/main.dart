af7f3243202f577116a465d5cdf3242271ce93a2
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-14 19:44:31 +0300

    rebuild game logic

--- lib/main.dart
+++ lib/main.dart
@@ -38,35 +38,45 @@ int createRandomNum() {
 }
 void makeGameLogic(RockPaperScissors playerChoice, RockPaperScissors computerChoice) {
   if (playerChoice.index == computerChoice.index) {
-    stdout.write("\nIt was a tie!\n");
+      stdout.write("\nIt was a tie!\n");
   } else if ((playerChoice.index == 1 && computerChoice.index == 0) ||
               (playerChoice.index == 0 && computerChoice.index == 2) ||
               (playerChoice.index == 2 && computerChoice.index == 1)) {
       stdout.write("\nYou win! Computer lose...\n");
-  }
-  else if ((computerChoice.index == 1 && playerChoice.index == 0) ||
-      (computerChoice.index == 0 && playerChoice.index == 2) ||
-      (computerChoice.index == 2 && playerChoice.index == 1)) {
-    stdout.write("\nComputer win! You lose...\n");
+  }  else {
+      stdout.write("\nComputer win! You lose...\n");
   }
 }
 
 int main() {
-  stdout.write('Select:\n1 - Rock, 2 - Paper, 3 - Scissors\nEnter: ');
-  final int playerIndex;
-  try {
-     playerIndex = int.parse(stdin.readLineSync() ?? '0');
+  int gameContinue = 1;
+  while (gameContinue == 1) {
+    stdout.write("--------------------------------------------------\n");
+    stdout.write('Select:\n1 - Rock, 2 - Paper, 3 - Scissors\nEnter: ');
+    final int playerIndex;
+    try {
+      playerIndex = int.parse(stdin.readLineSync() ?? '0');
       if (playerIndex < 1 || playerIndex > 3) {
         throw PlayerMoveIndexException();
       }
-  } on PlayerMoveIndexException catch(e) {
-    stdout.write(e.getErrorMessage());
-    return 0;
-  }
-  final playerMove = selectAttribute(playerIndex);
-  final computerMove = selectAttribute(createRandomNum());
+    } on PlayerMoveIndexException catch(e) {
+      stdout.write(e.getErrorMessage());
+      return 0;
+    }
+    final playerMove = selectAttribute(playerIndex);
+    final computerMove = selectAttribute(createRandomNum());
 
-  stdout.write("\nYour choice: ${playerMove.name} \nComputer choice: ${computerMove.name} \n");
-  makeGameLogic(playerMove, computerMove);
+    stdout.write("\nYour choice: ${playerMove.name} \nComputer choice: ${computerMove.name} \n");
+    makeGameLogic(playerMove, computerMove);
+
+    stdout.write("--------------------------------------------------\n");
+    stdout.write('Select:\n1 - Play game, 2 - Exit\nEnter: ');
+    gameContinue = int.parse(stdin.readLineSync() ?? '0');
+    if (gameContinue == 2) {
+      gameContinue++;
+      stdout.write('\nEnd game\n');
+    }
+  }
+
   return 0;
 }
